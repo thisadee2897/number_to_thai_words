3d566dea0ca49267e14b519f84bc1baac8201c75
odin.dev <thisadee.burom@gmail.com>
2025-01-30 16:44:09 +0700

    .

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,7 @@
 import 'package:number_to_thai_words/number_to_thai_words.dart';
 
 void main() {
-  final number = 123456.00;
-  final thaiWords = NumberToThaiWords.convert(number);
+  final double number = 123456.00;
+  final String thaiWords = NumberToThaiWords.convert(number);
   print('The number $number in Thai words is: $thaiWords');
 }
--- lib/number_to_thai_words.dart
+++ lib/number_to_thai_words.dart
@@ -1,9 +1,18 @@
 library number_to_thai_words;
 
+/// This class provides the functionality to convert numbers to Thai words.
+/// It includes a method to convert an integer into its Thai representation.
 class NumberToThaiWords {
-  static const List<String> _units = ['', 'หนึ่ง', 'สอง', 'สาม', 'สี่', 'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า'];
-  static const List<String> _scales = ['', 'สิบ', 'ร้อย', 'พัน', 'หมื่น', 'แสน', 'ล้าน'];
-
+  static const List<String> _units = <String>['', 'หนึ่ง', 'สอง', 'สาม', 'สี่', 'ห้า', 'หก', 'เจ็ด', 'แปด', 'เก้า'];
+  static const List<String> _scales = <String>['', 'สิบ', 'ร้อย', 'พัน', 'หมื่น', 'แสน', 'ล้าน'];
+/// Converts an integer number into its Thai word representation.
+///
+/// This method is a helper function used by the `convert` method to convert
+/// integer numbers into their corresponding Thai words. It handles scales like
+/// 'สิบ', 'ร้อย', 'พัน', etc., and formats the number accordingly.
+///
+/// - Parameter number: The integer number to be converted.
+/// - Returns: The Thai words representation of the number.
   static String _convertNumberToThaiWords(int number) {
     String words = '';
     int scaleIndex = 0;
@@ -41,6 +50,14 @@ class NumberToThaiWords {
     return words;
   }
 
+  /// Converts a numeric amount (in Baht and Satang) to its Thai words representation.
+  ///
+  /// This method takes a double value representing an amount in Baht and Satang
+  /// and converts it into Thai words. For example, the value `123.45` would be converted
+  /// to "หนึ่งร้อยยี่สิบสามบาทสี่สิบห้าสตางค์".
+  ///
+  /// - Parameter amount: The numeric amount to be converted.
+  /// - Returns: The Thai words representation of the amount.
   static String convert(double amount) {
     int baht = amount.floor();
     int satang = ((amount - baht) * 100).round();
