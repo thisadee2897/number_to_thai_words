b13a88fd9826e1b265be5cbfc24c6fa35828194c
odin.dev <thisadee.burom@gmail.com>
2025-01-31 10:51:35 +0700

    chang description

--- example/main.dart
+++ example/main.dart
@@ -4,5 +4,6 @@ import 'package:number_to_thai_words/number_to_thai_words.dart';
 void main() {
   const double number = 123456.00;
   final String thaiWords = NumberToThaiWords.convert(number);
-  if (kDebugMode) print(thaiWords); // หนึ่งแสนยี่หมื่นสามพันสี่ร้อยห้าสิบหกบาทถ้วน
+  if (kDebugMode)
+    print(thaiWords); // หนึ่งแสนยี่หมื่นสามพันสี่ร้อยห้าสิบหกบาทถ้วน
 }
